32bea32b4338ef8d128e1d78efdf2e99ce7ef5cc
Corey Alexander <coreyja@gmail.com>
2022-02-25 21:00:50 -0500

    Clippy and write comments

--- src/main.rs
+++ src/main.rs
@@ -98,11 +98,16 @@ fn handle_end(end_req: Json<GameState>) -> Status {
 
 #[launch]
 fn rocket() -> _ {
+    // Lots of web hosting services expect you to bind to the port specified by the `PORT`
+    // environment variable. However, Rocket looks at the `ROCKET_PORT` environment variable.
+    // If we find a value for `PORT`, we set `ROCKET_PORT` to that value.
     if let Ok(port) = env::var("PORT") {
         env::set_var("ROCKET_PORT", &port);
     }
 
-    if let Err(_) = env::var("RUST_LOG") {
+    // We default to 'info' level logging. But if the `RUST_LOG` environment variable is set,
+    // we keep that value instead.
+    if env::var("RUST_LOG").is_err() {
         env::set_var("RUST_LOG", "info");
     }
 
